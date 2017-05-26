8dc6c0db60ff0f6783da89451d2efcbf1122ea78
Anatoly Zhmur <Anatoly.Zhmur@broadridge.net>
2017-05-26 12:58:59 +0300

    Initial commit

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<SingleEnumerable>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class SingleEnumerable
+    {
+        private static readonly string value = "value";
+
+        [Benchmark]
+        public void Array()
+        {
+            foreach (var i in GetArray())
+            {
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private IEnumerable<string> GetArray()
+        {
+            return new[] { value };
+        }
+
+        [Benchmark]
+        public void EnumerableRepeat()
+        {
+            foreach (var i in GetEnumerableRepeat())
+            {
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private IEnumerable<string> GetEnumerableRepeat()
+        {
+            return Enumerable.Repeat(value, 1);
+        }
+
+        [Benchmark]
+        public void Yield()
+        {
+            foreach (var i in GetYield())
+            {
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private IEnumerable<string> GetYield()
+        {
+            yield return value;
+        }
+
+        [Benchmark]
+        public void SingleEnumerator()
+        {
+            foreach (var i in GetSingleEnumerator())
+            {
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private IEnumerable<string> GetSingleEnumerator()
+        {
+            return new SingleEnumerable<string>(value);
+        }
+    }
+
+    public sealed class SingleEnumerable<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        private T _value;
+        private int _state;
+
+        public SingleEnumerable(T value)
+        {
+            _value = value;
+            _state = 0;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (_state == 0)
+            {
+                _state = 1;
+                return this;
+            }
+
+            return new SingleEnumerable<T>(_value);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            switch (_state)
+            {
+                case 0:
+                case 1:
+                    _state = 2;
+                    return true;
+
+                case 2:
+                    _state = 3;
+                    return false;
+
+                default:
+                    return false;
+            }
+        }
+
+        public void Reset()
+        {
+            _state = 1;
+        }
+
+        public T Current
+        {
+            get
+            {
+                if (_state == 2)
+                    return _value;
+
+                throw new InvalidOperationException();
+            }
+        }
+
+        object IEnumerator.Current => Current;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+https://stackoverflow.com/questions/1577822/passing-a-single-item-as-ienumerablet
+
+``` ini
+
+BenchmarkDotNet=v0.10.6, OS=Windows 10 Redstone 1 (10.0.14393)
+Processor=Intel Core i7-4770 CPU 3.40GHz (Haswell), ProcessorCount=8
+Frequency=3318387 Hz, Resolution=301.3512 ns, Timer=TSC
+  [Host]     : Clr 4.0.30319.42000, 64bit RyuJIT-v4.6.1648.0
+  DefaultJob : Clr 4.0.30319.42000, 64bit RyuJIT-v4.6.1648.0
+
+
+```
+ |           Method |     Mean |     Error |    StdDev |  Gen 0 | Allocated |
+ |----------------- |---------:|----------:|----------:|-------:|----------:|
+ |            Array | 24.49 ns | 0.1977 ns | 0.1753 ns | 0.0152 |      64 B |
+ | EnumerableRepeat | 29.71 ns | 0.1216 ns | 0.0879 ns | 0.0152 |      64 B |
+ |            Yield | 21.80 ns | 0.2598 ns | 0.2303 ns | 0.0076 |      32 B |
+ | SingleEnumerator | 17.65 ns | 0.1694 ns | 0.1584 ns | 0.0076 |      32 B |
